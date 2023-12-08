e9d3497e28315c0800bdf970dd0c2c7382471543
Akshith Kumar Sudharshanam <57088979+AkshithGit@users.noreply.github.com>
2023-12-07 16:02:44 -0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,19 @@
-def buildApp() {
-  echo "building the application"
+def buildJar() {
+    echo "Building the application"
+    sh 'mvn package'
 }
-def testApp() {
-  echo "testing the application"
+def buildImage() {
+     echo "Building the docker image"
+     withCredentials([
+          usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')
+              ]){
+                sh 'docker build -t darkmatterdocker/java-maven-app:1.1.0 .'
+                sh "echo $PASS | docker login -u $USER --password-stdin"
+                sh 'docker push darkmatterdocker/java-maven-app:1.1.0'
+                }
 }
 def deployApp() {
-  echo "deploying the application"
-  echo "deploying the version ${params.VERSION}"
+    echo "Deploying the application"
 }
+
 return this
